06e80f4a031f996548a5c34fc9a9f8c2a121f30b
allwyn <allwyn153@gmail.com>
2025-08-27 13:44:43 +0530

    Completed extract, anlyze and research steps in workflow

---
+++ src/workflow.py
@@ -0,0 +1,134 @@
+from typing import Dict,Any
+from langgraph.graph import StateGraph,END
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage,SystemMessage
+from .models import ResearchState,CompanyAnalysis,CompanyInfo
+from .firecrawl import FirecrawlService
+from .prompts import DeveloperToolsPrompts
+
+
+class workflow:
+    def __init__(self):
+        self.firecrawl = FirecrawlService()
+        self.llm = ChatGoogleGenerativeAI(
+            model="gemini-2.5-flash", temperature=0.1,
+        )
+        self.prompts = DeveloperToolsPrompts()
+        self.workflow = self._build_workflow()
+
+
+    def _build_workflow(self):
+        pass
+
+    def _extract_tools_step(self,state:ResearchState)->Dict[str,any]:
+        print(f"Finding Articles about: {state.query}")
+
+        article_query = f"{state.query} tools comparison best alternatives"
+        search_results = self.firecrawl.search_companies(article_query,num_results=3)
+
+        all_contents = ""
+        for result in search_results.data:
+            url = result.get("url","")
+            scraped = self.firecrawl.scrape_company_pages(url=url)
+
+            if scraped:
+                all_contents + scraped.markdown[:1500] + "\n\n"
+
+
+        messages = [
+            SystemMessage(
+                content=self.prompts.TOOL_EXTRACTION_SYSTEM
+            ),
+            HumanMessage(
+                content=self.prompts.tool_extraction_user(state.query,all_contents)
+            )
+        ]
+
+        try:
+            response = self.llm.invoke(messages)
+            tool_names = [
+                name.strip()
+                for name in response.content.strip().split("\n")
+                if name.strip()
+            ]
+
+            print(f"Extracted Tools: {', '.join(tool_names[:5])}")
+            return {"Extracted Tools": tool_names}
+        except Exception as e:
+            print("extract tools error", e)
+            return {"Extracted Tools": []}
+        
+    
+    def _analyze_company_content(self,company_name:str,content:str)->CompanyAnalysis:
+        structured_llm = self.llm.with_structured_output(CompanyAnalysis)
+
+        messages = [
+            SystemMessage(content=self.prompts.TOOL_ANALYSIS_SYSTEM),
+            HumanMessage(content=self.prompts.tool_analysis_user(company_name,content))
+        ]
+
+        try:
+            analysis = structured_llm.invoke(messages)
+            return analysis
+        except Exception as e:
+            print("Analyze company content error",e)
+            return CompanyAnalysis(
+                pricing_model="unknown",
+                is_open_source=None,
+                tech_stack=[],
+                description="unknown",
+                api_available=None,
+                language_support=[],
+                integration_capabilities=[]
+            )
+        
+    def _research_step(self,state:ResearchState)->Dict[str,Any]:
+        extracted_tools = getattr(state,"extracted_tools",[])
+
+        if not extracted_tools:
+            print("no tools extracted, Falling back to direct Search")
+            search_results = self.firecrawl.search_companies(state.query,num_results=3)
+
+            tool_names=[
+                result.get("metadata",{}).get("title","unknown")
+                for result in search_results.data
+            ]
+
+        else:
+            tool_names = extracted_tools[:4]
+
+        print(f"Researching Specific Tools: {', '.join(tool_names)}")
+
+        companies = []
+
+        for tool_name in tool_names:
+            tool_search_results = self.firecrawl.search_companies(tool_name + "Official site",num_results=1)
+
+            if tool_search_results:
+                result = tool_search_results.data[0]
+                url = result.get("url","")
+
+                company = CompanyInfo(
+                    name=tool_name,
+                    description=result.get("markdown",""),
+                    website=url,
+                    tech_stack=[],
+                    competitors=[]
+                )
+
+                scraped = self.firecrawl.scrape_company_pages(url)
+                if scraped:
+                    content = scraped.markdown
+                    analysis = self._analyze_company_content(company_name=company.name,content=content)
+
+                    company.pricing_model = analysis.pricing_model
+                    company.is_open_source = analysis.is_open_source
+                    company.tech_stack = analysis.tech_stack
+                    company.description = analysis.description
+                    company.api_available = analysis.api_available
+                    company.language_support = analysis.language_support
+                    company.integration_capabilities = analysis.integration_capabilities
+
+                companies.append(company)
+
+        return {"companies" : companies}
